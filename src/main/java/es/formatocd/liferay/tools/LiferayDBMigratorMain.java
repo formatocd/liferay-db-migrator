e1474f72b0994b7abfcffa8f3038914eeba5fd59
Carlos Durán <formatocd@proton.me>
2026-06-12 22:50:19 +0200

    Main class adjust

--- src/main/java/es/formatocd/liferay/tools/LiferayDBMigratorMain.java
+++ src/main/java/es/formatocd/liferay/tools/LiferayDBMigratorMain.java
@@ -3,7 +3,6 @@ package es.formatocd.liferay.tools;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
 
 public class LiferayDBMigratorMain {
 
